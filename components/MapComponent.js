48737a2d5c381886779ea4305f6237ee44936788
Grizvok <burninglovedesign@gmail.com>
2018-09-18 20:53:29 -0400

    google map integration

--- components/MapComponent.js
+++ components/MapComponent.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
-import GoogleMapReact from 'google-map-react';
- 
+import GoogleMapReact from "google-map-react";
+
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
- 
+
 class MapComponent extends React.Component {
   static defaultProps = {
     center: {
@@ -12,25 +12,31 @@ class MapComponent extends React.Component {
     },
     zoom: 11
   };
- 
+
   render() {
     return (
       // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyAmfnpugo3zgohqIaMai0-2oGI6Afz0FVA"}}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text={'Kreyser Avrora'}
-          />
-        </GoogleMapReact>
+      <div className="columns">
+        <div className="column is-centered is-offset-one-quarter is-half">
+          <div style={{ height: "50vh", width: "100%" }}>
+            <GoogleMapReact
+              bootstrapURLKeys={{
+                key: "AIzaSyAmfnpugo3zgohqIaMai0-2oGI6Afz0FVA"
+              }}
+              defaultCenter={this.props.center}
+              defaultZoom={this.props.zoom}
+            >
+              <AnyReactComponent
+                lat={59.955413}
+                lng={30.337844}
+                text={"Kreyser Avrora"}
+              />
+            </GoogleMapReact>
+          </div>
+        </div>
       </div>
     );
   }
 }
- 
+
 export default MapComponent;
